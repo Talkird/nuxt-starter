7d0f1574a15b78be86389221f2a3883c2bf0f687
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-06-23 11:23:35 -0300

    i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@prisma/nuxt"],
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@prisma/nuxt", "@nuxtjs/i18n"],
+
+  i18n: {
+    defaultLocale: "en",
+    locales: [{ code: "en", name: "English", file: "en.json" }],
+  },
+
   css: ["~/main.css"],
 });
